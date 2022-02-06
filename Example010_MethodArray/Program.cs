2ba2878f972732b38fef4a1f99d7ccf53126593d
Max <lmaxvv@gmail.com>
2022-02-06 14:06:46 +0900

    Example 010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 16, 29, 37, 44, 15, 26, 75, 81 };
+
+int n = array.Length;
+int find = 15;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index = index++;
+}
